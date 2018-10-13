c52acb6788e31e131cf84301c83bde4da7d3eb8f
nishita goyal <goyalnishita07@gmail.com>
2018-10-13 12:52:43 +0530

    Final commit

---
+++ app/src/main/java/com/example/goyal/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.goyal.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //setContentView is a pre-defined function
+
+        TextView textView = findViewById(R.id.textview)
+    }
+    public void ButtonClick(View view) {
+    }
+    }
+}
